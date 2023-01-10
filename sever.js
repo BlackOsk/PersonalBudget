47b54960178ab512f5cb2e2424ef838269a04fb6
Black Oak <kkjiuyy@gmail.com>
2023-01-10 17:36:05 +0800

    transfer between envelops

--- sever.js
+++ sever.js
@@ -2,15 +2,9 @@ const express = require('express');
 const { get } = require('http');
 const app = express();
 
-let envelopes = [
-    {
-        Gas: 100,
-        Clothing:200,
-        DiningOut: 300,
-    }
-];
+let envelopes = [];
 
-let totalBudget = 600;
+let totalBudget = 0;
 
 
 
@@ -18,13 +12,13 @@ app.get('',(req, res, next) => {
     res.send('Hellow World!');
 })
 
-app.post('/totalbudget', (req, res, next) => {
-    const recevedBudget = req.query;
+app.post('/totalbudget/:number', (req, res, next) => {
+    const recevedBudget = Number(req.params.number);
     if(recevedBudget){
-        totalBudget = Number(recevedBudget['totalbudget']);
+        totalBudget = recevedBudget;
         res.send(totalBudget.toString());
     }else {
-        res.status(400).send;
+        res.status(400).send();
     }
 })
 
@@ -32,7 +26,7 @@ app.post('/envelops', (req, res, next) => {
     const recevedEnvelops = req.query;
     if(recevedEnvelops) {
         envelopes.push(recevedEnvelops);
-        res.status(201).send(envelopes[0]);
+        res.status(201).send(envelopes);
     }else {
         res.status(400).send;
     }
@@ -48,16 +42,17 @@ app.get('/envelops/:envname',(req, res, next) => {
     }
 })
 
-app.put('/envelops/cost',(req, res, next) => {
-    const costEnvelop = req.query.costEnvelop;
-    const costMoney = Number(req.query.costMoney);
+app.put('/envelops/cost/:costEnvelop/:costMoney',(req, res, next) => {
+    const costEnvelop = req.params.costEnvelop;
+    const costMoney = Number(req.params.costMoney);
     if(envelopes[0][costEnvelop]- costMoney >= 0){
         envelopes[0][costEnvelop] -= costMoney;
         totalBudget -= costMoney;
         res.send(envelopes[0][costEnvelop].toString());
     } else {
         envelopes[0][costEnvelop] = 0;
-        res.send('You have spend all the money in the envelop!')
+        totalBudget -= costMoney;
+        res.send('You have spend all the money in the envelop and spend extra Money!')
     }
     
 })
@@ -72,6 +67,19 @@ app.delete('/envelops/:envname', (req, res, next) => {
     } else {
         res.status(404).send('Envelop not found!');
     }    
+})
+
+app.post('/envelops/transfer/:from/:to/:number', (req, res, next) => {
+    const transFrom = req.params.from;
+    const transTo = req.params.to;
+    const transNumber = Number(req.params.number);
+    if(envelopes[0][transFrom] >= transNumber){
+        envelopes[0][transFrom] -= transNumber;
+        envelopes[0][transTo] += transNumber;
+        res.send(envelopes[0]);
+    }else {
+        res.send(`You don't have ENOUGH money in ${transFrom}!`);
+    }
 })
 
 app.get('/envelops',(req, res, next)=> {
