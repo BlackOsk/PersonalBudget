27bf21fe9ceecd8264afd1da3324e899e1bbb734
Black Oak <kkjiuyy@gmail.com>
2023-01-10 16:21:32 +0800

    Update specific envelopes and balances

--- sever.js
+++ sever.js
@@ -12,6 +12,16 @@ app.get('',(req, res, next) => {
     res.send('Hellow World!');
 })
 
+app.post('/totalbudget', (req, res, next) => {
+    const recevedBudget = req.query;
+    if(recevedBudget){
+        totalBudget = Number(recevedBudget['totalbudget']);
+        res.send(totalBudget.toString());
+    }else {
+        res.status(400).send;
+    }
+})
+
 app.post('/envelops', (req, res, next) => {
     const recevedEnvelops = req.query;
     if(recevedEnvelops) {
@@ -22,31 +32,36 @@ app.post('/envelops', (req, res, next) => {
     }
 })
 
+
 app.get('/envelops/:envname',(req, res, next) => {
     const envelopContent = envelopes[0][req.params.envname];
     if(envelopContent){
-        res.send(`${req.params.envname}: ${envelopContent}`);
+        res.send(envelopContent);
     }else {
         res.status(404).send('Envelop not found.');
     }
+})
+
+app.put('/envelops/cost',(req, res, next) => {
+    const costEnvelop = req.query.costEnvelop;
+    const costMoney = Number(req.query.costMoney);
+    if(envelopes[0][costEnvelop]- costMoney >= 0){
+        envelopes[0][costEnvelop] -= costMoney;
+        totalBudget -= costMoney;
+        res.send(envelopes[0][costEnvelop].toString());
+    } else {
+        envelopes[0][costEnvelop] = 0;
+        res.send('You have spend all the money in the envelop!')
+    }
+    
 })
 
 app.get('/envelops',(req, res, next)=> {
     res.send(envelopes);
 })
 
-app.post('/envelops/totalbudget', (req, res, next) => {
-    const recevedBudget = req.query;
-    if(recevedBudget){
-        totalBudget = recevedBudget['totalbudget'];
-        res.status(201).send(totalBudget);
-    }else {
-        res.status(400).send;
-    }
-})
-
-app.get('/envelops/totalbudget',(req, res, next)=> {
-    res.send(totalBudget);
+app.get('/totalbudget',(req, res, next)=> {
+    res.send(totalBudget.toString());
 })
 
 const PORT = 3000;
